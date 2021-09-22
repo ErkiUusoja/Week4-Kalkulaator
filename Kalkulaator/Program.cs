fc2ba6ce399bef6eed798ddc62fa59ffa6f8cb5e
Opilane TTHK <opilane@tthk.ee>
2021-09-22 12:30:34 +0300

    first commit

---
+++ Kalkulaator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Kalkulaator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer;
+            string ops;
+
+            Console.WriteLine("\t\t\tconsole Calculator tutorial in c#\r");
+            Console.WriteLine("\t\t\t---------------------------------\r");
+
+            Console.WriteLine("\t\t\tEnter first number\n");
+            firstnum = double.Parse(Console.ReadLine());
+            Console.ReadKey();
+
+            Console.WriteLine("\t\t\tselect an operator : (+, -, *, /, %)\n");
+            ops = (Console.ReadLine());
+            Console.ReadKey();
+
+            Console.WriteLine("\t\t\tEnter second number\n");
+            secondnum = double.Parse("\t\t\t" + Console.ReadLine());
+
+            if(ops == "+")
+            {
+                answer = firstnum + secondnum;
+                Console.Write("\n" + answer);
+            }
+            if (ops == "-")
+            {
+                answer = firstnum - secondnum;
+                Console.Write("\n" + answer);
+            }
+            if (ops == "*")
+            {
+                answer = firstnum * secondnum;
+                Console.Write("\n" + answer);
+            }
+            if (ops == "/")
+            {
+                answer = firstnum / secondnum;
+                Console.Write("\n" + answer);
+            }
+            if (ops == "%")
+            {
+                answer = firstnum % secondnum;
+                Console.Write("\n" + answer);
+            }
+
+            Console.Write("\n" + "press an key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
